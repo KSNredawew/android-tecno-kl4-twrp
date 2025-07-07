c380ae1cae6fee6231030b8f0062df5654bc6f12
KSN <redawewrq12@gmail.com>
2025-07-07 18:00:20 +0300

    Update twrp_KL4.mk

--- twrp_KL4.mk
+++ twrp_KL4.mk
@@ -10,10 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from tssi_arm64_tecno device
-$(call inherit-product, device/tecno/tssi_arm64_tecno/device.mk)
+$(call inherit-product, device/tecno/KL4/device.mk)
 
 PRODUCT_DEVICE := KL4
 PRODUCT_NAME := twrp_KL4
