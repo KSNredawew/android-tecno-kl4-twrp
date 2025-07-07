bd0014eb583099492f3fc7ae8f079efddd0d7605
KSN <redawewrq12@gmail.com>
2025-07-07 18:23:01 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_KL4.mk
 
-COMMON_LUNCH_CHOICES := \ 
-    twrp_KL4-user \
-    twro_KL4-userdebug \
-    twrp_KL4-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_KL4-eng 
