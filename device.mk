346747318bbfde6d0ffb70395fcf732121504275
KSN <redawewrq12@gmail.com>
2025-07-07 20:55:16 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,12 +1,21 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The TWRP Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 LOCAL_PATH := device/tecno/KL4
-# A/B
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -15,16 +24,43 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl-1.2 
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.2-service \
+
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
+
+ # Dynamic Partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true   
+
+TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules)\")
+ 
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl-recovery \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+
+
+PRODUCT_PACKAGES += \
+    bootctrl.recovery \
+    unisoc.bootctrl
+
 
 PRODUCT_PACKAGES += \
     bootctrl.ums9230E
+    
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.ums9230E \
-    libgptutils \
-    libz \
-    libcutils
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client 
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -33,5 +69,23 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl-default
+    mkfs.erofs.recovery \
+    dump.erofs.recovery \
+    fsck.erofs.recovery
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 34
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# A/B
+
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
