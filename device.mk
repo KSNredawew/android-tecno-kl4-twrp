7d0de408c8dfa45054673e31439e07c1dea32a6a
KSN <redawewrq12@gmail.com>
2025-07-16 03:05:16 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,74 +16,17 @@
 
 LOCAL_PATH := device/tecno/KL4
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.2-service \
-
-
-# Health HAL
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
-
- # Dynamic Partition
-PRODUCT_USE_DYNAMIC_PARTITIONS := true   
- 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl-recovery \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-
-
-PRODUCT_PACKAGES += \
-    bootctrl.recovery \
-    unisoc.bootctrl
-
-
-PRODUCT_PACKAGES += \
-    bootctrl.ums9230E
-    
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client 
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-
-
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl-1.2 \
+     
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl-default \
+    libhealthd.$(PRODUCT_PLATFORM)
 
 PRODUCT_PACKAGES += \
-    mkfs.erofs.recovery \
-    dump.erofs.recovery \
-    fsck.erofs.recovery
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 32
-PRODUCT_SHIPPING_API_LEVEL := 32
-
-# A/B
-
-
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-default \
     fastbootd
